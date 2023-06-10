7ecdbef33aa283679cc603d4bfffed850a34c6f0
阿P <oncwnuN0xSjty0jotKHWGgliyJtU@git.weixin.qq.com>
2023-06-10 20:47:49 +0800

    submit

--- chatgpt-base/src/main/java/com/hncboy/chatgpt/base/config/SaTokenConfig.java
+++ chatgpt-base/src/main/java/com/hncboy/chatgpt/base/config/SaTokenConfig.java
@@ -24,20 +24,20 @@ public class SaTokenConfig implements WebMvcConfigurer {
         // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。
         registry.addInterceptor(new SaInterceptor(handle -> {
                     // 管理端接口都必须管理端登录
-                    SaRouter.match("/admin/**").check(r -> StpAdminUtil.checkLogin());
+                    SaRouter.match("/api/admin/**").check(r -> StpAdminUtil.checkLogin());
                     // 非管理端接口都必须 front 用户登录
-                    SaRouter.notMatch("/admin/**").check(r -> StpUtil.checkLogin());
+                    SaRouter.notMatch("/api/admin/**").check(r -> StpUtil.checkLogin());
                 }))
                 // 放行管理端登录接口
-                .excludePathPatterns("/admin/sys_user/login")
+                .excludePathPatterns("/api/admin/sys_user/login")
                 // 放行用户端校验邮箱验证码
-                .excludePathPatterns("/user/verify_email_code")
+                .excludePathPatterns("/api/user/verify_email_code")
                 // 放行用户端邮箱注册
-                .excludePathPatterns("/user/register/email")
+                .excludePathPatterns("/api/user/register/email")
                 // 放行用户端图形验证码
-                .excludePathPatterns("/user/get_pic_code")
+                .excludePathPatterns("/api/user/get_pic_code")
                 // 放行用户端邮箱登录
-                .excludePathPatterns("/user/login/email")
+                .excludePathPatterns("/api/user/login/email")
                 // swagger 放行
                 .excludePathPatterns("/swagger-ui/**")
                 .excludePathPatterns("/v3/api-docs/**");
