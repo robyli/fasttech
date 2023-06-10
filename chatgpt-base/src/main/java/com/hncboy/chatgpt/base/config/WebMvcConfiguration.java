89112f1ce2ddd9ad62c4e709a9d5994f17c7e174
阿P <oncwnuN0xSjty0jotKHWGgliyJtU@git.weixin.qq.com>
2023-06-10 15:55:33 +0800

    submit

--- chatgpt-base/src/main/java/com/hncboy/chatgpt/base/config/WebMvcConfiguration.java
+++ chatgpt-base/src/main/java/com/hncboy/chatgpt/base/config/WebMvcConfiguration.java
@@ -2,8 +2,11 @@ package com.hncboy.chatgpt.base.config;
 
 import com.hncboy.chatgpt.base.annotation.ApiAdminRestController;
 import com.hncboy.chatgpt.base.constant.ApplicationConstant;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -14,9 +17,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+    @Value("${web.upload-path}")
+    private String baseUploadPath;
+
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix(ApplicationConstant.ADMIN_PATH_PREFIX, c -> c.isAnnotationPresent(ApiAdminRestController.class));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("file:"+ baseUploadPath);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:index.html");
     }
 }
 
